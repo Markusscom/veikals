b46d093b377bf7ce5987b6a6316ac3b8b54cac17
markusscom <markusskalendra112@inbox.lv>
2026-04-21 13:58:03 +0300

    Kļūdu Labojums: Salaboju Dublēšanas kļūdu ko veica MI Aģents

--- public/index.php
+++ public/index.php
@@ -33,26 +33,3 @@ switch ($requestUri) {
         echo "<h1>404 - Lapa nav atrasta</h1>";
         break;
 }
-
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-switch ($requestUri) {
-    case '/':
-        echo "<h1>Veikals</h1>";
-        echo "<a href='/customers'>Klienti</a><br>";
-        echo "<a href='/orders'>Pasūtījumi</a><br>";
-        break;
-
-    case '/customers':
-        $customerController->index();
-        break;
-
-    case '/orders':
-        $orderController->index();
-        break;
-
-    default:
-        http_response_code(404);
-        echo "<h1>404 - Lapa nav atrasta</h1>";
-        break;
-}
